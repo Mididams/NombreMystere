ccbeb8ae0d444695d61e68a58df766167384b9ee
Mididams <mididams@gmail.com>
2024-03-30 23:16:21 +0100

    Première version

--- obj/Debug/net8.0/NombreMystere.AssemblyInfo.cs
+++ obj/Debug/net8.0/NombreMystere.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NombreMystere")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+293a6e43bce0aac3e9ee095d8fc03263e3bbdda6")]
 [assembly: System.Reflection.AssemblyProductAttribute("NombreMystere")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NombreMystere")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
