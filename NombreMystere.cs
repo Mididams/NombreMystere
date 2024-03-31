c8b4543c026280bbc03fa0321a008f3d70d88445
Mididams <mididams@gmail.com>
2024-03-31 12:11:30 +0200

    simplifier avec methode NombreMystere

--- NombreMystere.cs
+++ NombreMystere.cs
@@ -2,6 +2,7 @@
 
 string? readResult;
 string entreeUtilisateur = "";
+int nbMystere = 0;
 int nbUtilisateur = 0;
 int compteur = 0;
 
@@ -26,205 +27,80 @@ do
             break;
 
         case "1":
-            Random nbAleatoire = new Random();
-            int nbMystere = nbAleatoire.Next(1, 1000);
-
-            do
-            {
-                Console.Clear();
-                Console.WriteLine("Bienvenue dans \"Le nombre Mystère\"");
-                Console.WriteLine("Trouve le nombre mystère entre 1 et 999.");
-                Console.WriteLine("Je te dirai si c'est plus ou moins!");
-                Console.WriteLine("A toi de jouer...\n ");
-                //Console.WriteLine("Nombre mystère = " + nbMystere);
-
-                do
-                {
-                    readResult = Console.ReadLine();
-                    if (readResult != null)
-                    {
-                        entreeUtilisateur = readResult;
-                        compteur++;
-                    }
-
-
-                    bool validNumber = false;
-                    validNumber = int.TryParse(entreeUtilisateur, out nbUtilisateur);
-                    if (validNumber == false)
-                    {
-                        Console.WriteLine("Entre un nombre, pas des lettres!");
-                        continue;
-                    }
-
-                    if (nbUtilisateur < 1 || nbUtilisateur > 999)
-                    {
-                        Console.WriteLine("Ton nombre n'est pas compris entre 1 et 999");
-                        continue;
-                    }
-
-                    if (nbUtilisateur < nbMystere)
-                        Console.WriteLine("C'est plus!\n");
-
-                    if (nbUtilisateur > nbMystere)
-                        Console.WriteLine("C'est moins!\n");
-
-                } while (nbUtilisateur != nbMystere);
-                Console.WriteLine($"BRAVO! Tu as trouvé le nombre mystère en {compteur} coups!");
-                readResult = Console.ReadLine();
-                return;
-
-            } while (entreeUtilisateur != "0" && nbUtilisateur != nbMystere);
+            NumeroMystere(999);
+            break;
 
         case "2":
-            Random nbAleatoire2 = new Random();
-            int nbMystere2 = nbAleatoire2.Next(1, 10000);
-
-            do
-            {
-                Console.Clear();
-                Console.WriteLine("Bienvenue dans \"Le nombre Mystère\"");
-                Console.WriteLine("Trouve le nombre mystère entre 1 et 9 999.");
-                Console.WriteLine("Je te dirai si c'est plus ou moins!");
-                Console.WriteLine("A toi de jouer...\n ");
-                //Console.WriteLine("Nombre mystère = " + nbMystere2);
-
-                do
-                {
-                    readResult = Console.ReadLine();
-                    if (readResult != null)
-                    {
-                        entreeUtilisateur = readResult;
-                        compteur++;
-                    }
-
-                    bool validNumber = false;
-                    validNumber = int.TryParse(entreeUtilisateur, out nbUtilisateur);
-                    if (validNumber == false)
-                    {
-                        Console.WriteLine("Entre un nombre, pas des lettres!");
-                        continue;
-                    }
-
-                    if (nbUtilisateur < 1 || nbUtilisateur > 9999)
-                    {
-                        Console.WriteLine("Ton nombre n'est pas compris entre 1 et 9 999");
-                        continue;
-                    }
-
-                    if (nbUtilisateur < nbMystere2)
-                        Console.WriteLine("C'est plus!\n");
-
-                    if (nbUtilisateur > nbMystere2)
-                        Console.WriteLine("C'est moins!\n");
-
-                } while (nbUtilisateur != nbMystere2);
-                Console.WriteLine($"BRAVO! Tu as trouvé le nombre mystère en {compteur} coups!");
-                readResult = Console.ReadLine();
-                return;
-
-            } while (entreeUtilisateur != "0" && nbUtilisateur != nbMystere2);
+            NumeroMystere(9999);
+            break;
 
 
         case "3":
-            Random nbAleatoire3 = new Random();
-            int nbMystere3 = nbAleatoire3.Next(1, 100000);
-
-            do
-            {
-                Console.Clear();
-                Console.WriteLine("Bienvenue dans \"Le nombre Mystère\"");
-                Console.WriteLine("Trouve le nombre mystère entre 1 et 99 999.");
-                Console.WriteLine("Je te dirai si c'est plus ou moins!");
-                Console.WriteLine("A toi de jouer...\n ");
-                //Console.WriteLine("Nombre mystère = " + nbMystere3);
-
-                do
-                {
-                    readResult = Console.ReadLine();
-                    if (readResult != null)
-                    {
-                        entreeUtilisateur = readResult;
-                        compteur++;
-                    }
-
-                    bool validNumber = false;
-                    validNumber = int.TryParse(entreeUtilisateur, out nbUtilisateur);
-                    if (validNumber == false)
-                    {
-                        Console.WriteLine("Entre un nombre, pas des lettres!");
-                        continue;
-                    }
-
-                    if (nbUtilisateur < 1 || nbUtilisateur > 99999)
-                    {
-                        Console.WriteLine("Ton nombre n'est pas compris entre 1 et 99 999");
-                        continue;
-                    }
-
-                    if (nbUtilisateur < nbMystere3)
-                        Console.WriteLine("C'est plus!\n");
-
-                    if (nbUtilisateur > nbMystere3)
-                        Console.WriteLine("C'est moins!\n");
-
-                } while (nbUtilisateur != nbMystere3);
-                Console.WriteLine($"BRAVO! Tu as trouvé le nombre mystère en {compteur} coups!");
-                readResult = Console.ReadLine();
-                return;
-
-            } while (entreeUtilisateur != "0" && nbUtilisateur != nbMystere3);
+            NumeroMystere(99999);
+            break;
 
         case "4":
-            Random nbAleatoire4 = new Random();
-            int nbMystere4 = nbAleatoire4.Next(1, 1000000);
-
-            do
-            {
-                Console.Clear();
-                Console.WriteLine("Bienvenue dans \"Le nombre Mystère\"");
-                Console.WriteLine("Trouve le nombre mystère entre 1 et 999 999.");
-                Console.WriteLine("Je te dirai si c'est plus ou moins!");
-                Console.WriteLine("A toi de jouer...\n ");
-                //Console.WriteLine("Nombre mystère = " + nbMystere4);
-
-                do
-                {
-                    readResult = Console.ReadLine();
-                    if (readResult != null)
-                    {
-                        entreeUtilisateur = readResult;
-                        compteur++;
-                    }
-
-                    bool validNumber = false;
-                    validNumber = int.TryParse(entreeUtilisateur, out nbUtilisateur);
-                    if (validNumber == false)
-                    {
-                        Console.WriteLine("Entre un nombre, pas des lettres!");
-                        continue;
-                    }
-
-                    if (nbUtilisateur < 1 || nbUtilisateur > 999999)
-                    {
-                        Console.WriteLine("Ton nombre n'est pas compris entre 1 et 999 999");
-                        continue;
-                    }
-
-                    if (nbUtilisateur < nbMystere4)
-                        Console.WriteLine("C'est plus!\n");
-
-                    if (nbUtilisateur > nbMystere4)
-                        Console.WriteLine("C'est moins!\n");
-
-                } while (nbUtilisateur != nbMystere4);
-                Console.WriteLine($"BRAVO! Tu as trouvé le nombre mystère en {compteur} coups!");
-                readResult = Console.ReadLine();
-                return;
-
-            } while (entreeUtilisateur != "0" && nbUtilisateur != nbMystere4);
-
-        default:
+            NumeroMystere(999999);
             break;
     }
 
-} while (entreeUtilisateur != "0");
+} while (entreeUtilisateur != "0" && entreeUtilisateur != "stop" && nbMystere != nbUtilisateur);
+
+
+void NumeroMystere(int nbMax)
+{
+    Random nbAleatoire = new Random();
+    nbMystere = nbAleatoire.Next(1, nbMax);
+
+    do
+    {
+        Console.Clear();
+        Console.WriteLine("Bienvenue dans \"Le nombre Mystère\"");
+        Console.WriteLine($"Trouve le nombre mystère entre 1 et {nbMax:N0}.");
+        Console.WriteLine("Je te dirai si c'est plus ou moins!");
+        Console.WriteLine("Ecris stop pour arréter.");
+        Console.WriteLine("A toi de jouer...\n ");
+        //Console.WriteLine("Nombre mystère = " + nbMystere);
+
+        do
+        {
+            readResult = Console.ReadLine();
+            if (readResult != null)
+            {
+                entreeUtilisateur = readResult;
+                compteur++;
+            }
+
+            if (entreeUtilisateur == "stop")
+                return;
+
+            bool validNumber = false;
+            validNumber = int.TryParse(entreeUtilisateur, out nbUtilisateur);
+            if (validNumber == false)
+            {
+                Console.WriteLine("Entre un nombre, pas des lettres!\n");
+                compteur--;
+                continue;
+            }
+
+            if (nbUtilisateur < 1 || nbUtilisateur > nbMax)
+            {
+                Console.WriteLine($"Ton nombre n'est pas compris entre 1 et {nbMax:N0}");
+                compteur--;
+                continue;
+            }
+
+            if (nbUtilisateur < nbMystere)
+                Console.WriteLine("C'est plus!\n");
+
+            if (nbUtilisateur > nbMystere)
+                Console.WriteLine("C'est moins!\n");
+
+        } while (nbUtilisateur != nbMystere && entreeUtilisateur != "stop");
+
+        Console.WriteLine($"BRAVO! Tu as trouvé le nombre mystère en {compteur} coups!");
+        readResult = Console.ReadLine();
+        return;
+
+    } while (nbUtilisateur != nbMystere);
+}
